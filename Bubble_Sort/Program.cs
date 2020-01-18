80bba34c51c58ae491761e144bf90f1e2cea0943
Ray <raymondcheung8@users.noreply.github.com>
2020-01-18 14:22:42 +0000

    Create Program.cs

---
+++ Bubble_Sort/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bubble_Sort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a list to sort:");
+            string message = Console.ReadLine();                     
+            string[] arrayMessage = message.Split(',');
+
+            for (int i = 0; i <= arrayMessage.Length - 2; i++)
+            {
+                for (int j = 0; j <= arrayMessage.Length - 2 - i; j++)
+                {
+                    if (Convert.ToInt64(arrayMessage[j]) > (Convert.ToInt64(arrayMessage[j + 1])))
+                    {
+                        string temp = arrayMessage[j + 1];
+                        arrayMessage[j + 1] = arrayMessage[j];
+                        arrayMessage[j] = temp;
+                    }
+                }
+            }
+            foreach (string items in arrayMessage) Console.WriteLine(items);
+            Console.ReadKey();
+        }
+    }
+}
